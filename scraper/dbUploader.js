ac47e039af4ff2e631db4b4401a1ba8324fe3fb9
Nicola Ferracin <nicola.ferracin@gmail.com>
2019-12-22 17:15:47 +0100

    Write script to upload data to the db

---
+++ scraper/dbUploader.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql');
+require('dotenv').config();
+
+const restaurants = JSON.parse(require('fs').readFileSync(__dirname + '/restaurants.json', 'utf8'));
+
+const connection = mysql.createConnection({
+  host: process.env.MY_SQL_HOST,
+  port: process.env.MY_SQL_PORT,
+  user: process.env.MY_SQL_USER,
+  password: process.env.MY_SQL_PASSWORD,
+  database: process.env.MY_SQL_DATABASE,
+});
+
+connection.connect();
+
+restaurants.forEach(restaurant => {
+  connection.query('INSERT INTO restaurants SET ?', restaurant, function (error, results, fields) {
+    if (error) {
+      throw error;
+    }
+    console.log({ results, fields })
+  });
+})
+
+connection.end();
