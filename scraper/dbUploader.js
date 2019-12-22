d4f34a373acfa1c68155d8974571f963e45301bd
Nicola Ferracin <nicola.ferracin@gmail.com>
2019-12-22 22:37:46 +0100

    Move dbUploader to its own class

--- scraper/dbUploader.js
+++ scraper/dbUploader.js
@@ -1,24 +1,30 @@
 const mysql = require('mysql');
 require('dotenv').config(__dirname + '../.env');
 
-const restaurants = JSON.parse(require('fs').readFileSync(__dirname + '/restaurants.json', 'utf8'));
+class DBUploader {
+  constructor() {
+    this.connection = mysql.createConnection({
+      host: process.env.MY_SQL_HOST,
+      port: process.env.MY_SQL_PORT,
+      user: process.env.MY_SQL_USER,
+      password: process.env.MY_SQL_PASSWORD,
+      database: process.env.MY_SQL_DATABASE,
+    });
+  }
 
-const connection = mysql.createConnection({
-  host: process.env.MY_SQL_HOST,
-  port: process.env.MY_SQL_PORT,
-  user: process.env.MY_SQL_USER,
-  password: process.env.MY_SQL_PASSWORD,
-  database: process.env.MY_SQL_DATABASE,
-});
+  connect = () => this.connection.connect();
 
-connection.connect();
+  disconnect = () => this.connection.end();
 
-restaurants.forEach(restaurant => {
-  connection.query(`INSERT INTO ${process.env.MY_SQL_TABLE} SET ?`, restaurant, function (error, results, fields) {
-    if (error) {
-      throw error;
-    }
-  });
-})
+  uploadRestaurants = restaurants => {
+    restaurants.forEach(restaurant => {
+      this.connection.query(`INSERT INTO ${process.env.MY_SQL_TABLE} SET ?`, restaurant, error => {
+        if (error) {
+          throw error;
+        }
+      });
+    })
+  }
+}
 
-connection.end();
+module.exports = DBUploader;
