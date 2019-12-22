923c228ab27696ae8c91afc74ed872e73cc35abb
Nicola Ferracin <nicola.ferracin@gmail.com>
2019-12-22 21:48:50 +0100

    Turn scraper into a class and promisify it

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,60 +1,70 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
-const RateLimiter = require('limiter').RateLimiter;
-const limiter = new RateLimiter(1, 1500);
 
 const { baseUrl, query, itemsPerPage, ratingsMap, selectors, regEx } = require('./utils')
 
-const run = () => {
-  limiter.removeTokens(1, () => {
-    scrape(1);
-  });
-}
-
-const getTotalPages = $ => {
-  const totalItems = Number($('h1').text().match(regEx.totalItems)[0].replace(/[\.,]/g, ''));
-  return Math.ceil(totalItems / itemsPerPage);
-}
-
-const scrape = async (currentPage, totalPages = 1, restaurants = []) => {
-  const options = {
-    uri: baseUrl + query + currentPage,
-    transform: body => {
-      return cheerio.load(body);
-    }
-  };
-
-  const $ = await rp(options).catch((err) => {
-    delete err.response
-    console.log(err)
-  });
-
-  if (totalPages === 1) {
-    totalPages = getTotalPages($);
+class Scraper {
+  constructor() {
+    this.rateLimiterDelay = 1500;
+    this.restaurants = [];
+    this.totalPages = 1;
   }
 
-  const cards = $(selectors.cards);
-  cards.each(function () {
-    const $card = $(this)
-    const rating = ratingsMap[$card.find(selectors.rating).text().trim()];
-    const year = $card.find(selectors.year).text().trim();
-    const img = $card.find(selectors.img).html().match(regEx.img)[0];
-    const $title = $card.find(selectors.name);
-    const name = $title.text().trim();
-    const link = $title.attr('href');
-    const location = $card.find(selectors.location).text().trim();
-    const type = $card.find(selectors.type).text().trim();
-    const lat = $card.data('lat');
-    const lng = $card.data('lng');
+  run = async () => {
+    await this.scrape(1);
+  }
 
-    restaurants.push({ rating, year, img, name, link, location, type, lat, lng });
-  });
+  getRestaurants = () => this.restaurants;
 
-  if (currentPage <= totalPages) {
-    limiter.removeTokens(1, () => {
-      scrape(currentPage + 1, totalPages, restaurants);
+  setTotalPages = $ => {
+    const totalItems = Number($('h1').text().match(regEx.totalItems)[0].replace(/[\.,]/g, ''));
+    this.totalPages = Math.ceil(totalItems / itemsPerPage);
+  }
+
+  scrape = async (currentPage) => {
+    const that = this;
+    const start = new Date();
+    const options = {
+      uri: baseUrl + query + currentPage,
+      transform: body => {
+        return cheerio.load(body);
+      }
+    };
+
+    const $ = await rp(options).catch((err) => {
+      delete err.response
+      console.log(err)
     });
+
+    if (this.totalPages === 1) {
+      this.setTotalPages($);
+    }
+
+    const cards = $(selectors.cards);
+    cards.each(function () {
+      const $card = $(this)
+      const rating = ratingsMap[$card.find(selectors.rating).text().trim()];
+      const year = $card.find(selectors.year).text().trim();
+      const img = $card.find(selectors.img).html().match(regEx.img)[0];
+      const $title = $card.find(selectors.name);
+      const name = $title.text().trim();
+      const link = $title.attr('href');
+      const location = $card.find(selectors.location).text().trim();
+      const type = $card.find(selectors.type).text().trim();
+      const lat = $card.data('lat');
+      const lng = $card.data('lng');
+
+      that.restaurants.push({ rating, year, img, name, link, location, type, lat, lng });
+    });
+
+    const end = new Date();
+
+    await setTimeout(() => { }, this.rateLimiterDelay - (end - start))
+    if (currentPage <= 3) {
+      return this.scrape(currentPage + 1);
+    }
+    return;
   }
 }
 
-run();
+module.exports = Scraper;
