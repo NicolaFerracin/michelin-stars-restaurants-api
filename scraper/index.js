7478a09c080de795da8dd357e4ea3acd3fbb6ea2
Nicola Ferracin <nicola.ferracin@gmail.com>
2019-12-23 16:52:52 +0100

    Add logs to the scraper and enable multiple scraping to get more accurate data

--- scraper/index.js
+++ scraper/index.js
@@ -7,15 +7,37 @@ const run = async () => {
 
 const getRestaurants = async () => {
   const scraper = new Scraper();
-  await scraper.run();
-  scraper.removeDuplicates();
+
+  // Scraping can be flaky. Attempt scraping multiple times
+  for (let i = 0; i < 3; i++) {
+    console.log('Scraping attempt:', i + 1);
+
+    await scraper.run();
+    console.log(`Found ${scraper.getRestaurants().length} restaurants`);
+
+    scraper.removeDuplicates();
+    console.log(`Duplicates removed, ${scraper.getRestaurants().length} restaurants remained`);
+
+    console.log('==========');
+  }
   return scraper.getRestaurants();
 }
 
 const uploadRestaurantsToDb = (restaurants) => {
+  console.log('Connecting to the database');
   const dbUploader = new DBUploader();
   dbUploader.connect();
+
+  console.log('Clearing old data');
+  dbUploader.clearTable();
+
+  console.log('Resetting auto increment ID');
+  dbUploader.resetAutoIncrement();
+
+  console.log('Uploading new data');
   dbUploader.uploadRestaurants(restaurants);
+
+  console.log('Closing connection');
   dbUploader.disconnect();
 }
 
